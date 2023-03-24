dac6f51fa511f79f3047a85056fb54e9bc616b2e
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-03-24 11:36:17 -0400

    added and implemented ESlint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react'
-import "bootstrap/dist/css/bootstrap.min.css";
-import ColorSchemesExample from './NavBar/navBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import CurRates from './components/curRates';
-import './App.css'
+import './App.css';
 import Conversion from './components/conversions';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom" //using this for routing 
-import NavBar from './NavBar/navBar'; 
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //using this for routing
+import NavBar from './NavBar/navBar';
 
 function App() {
-
-  
-  return (
-    <div className="App">
-  <NavBar /> 
-  <Router> {/* parent element */}
-    <Routes>
-  <Route exact path ="/" element={<CurRates />} />  {/* Home directory */}
-    <Route exact path ="/conversions" element={<Conversion />} /> {/* Conversion page */}
-    </Routes>
-    </Router>
-    </div>
-  )
+	return (
+		<div className="App">
+			<NavBar />
+			<Router>
+				{' '}
+				{/* parent element */}
+				<Routes>
+					<Route exact path="/" element={<CurRates />} /> {/* Home directory */}
+					<Route exact path="/conversions" element={<Conversion />} />{' '}
+					{/* Conversion page */}
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/NavBar/navBar.jsx
+++ src/NavBar/navBar.jsx
@@ -3,20 +3,19 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar() {
-  return (
-    <>
-      <Navbar fixed="top" bg="danger" variant="dark" >
-        <Container>
-          <Navbar.Brand href="#">Currency Converter</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Current Rates</Nav.Link>
-            <Nav.Link href="/conversions">Conversions</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>      
-      
-    </>
-  );
+	return (
+		<>
+			<Navbar fixed="top" bg="danger" variant="dark">
+				<Container>
+					<Navbar.Brand href="#">Currency Converter</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="/">Current Rates</Nav.Link>
+						<Nav.Link href="/conversions">Conversions</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
 
 export default NavBar;
